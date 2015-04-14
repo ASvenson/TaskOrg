b8b0d857de541e99f9d9889fb3809ef3ac8cd3a5
flickasven <flicksven@yahoo.com>
2015-04-14 14:10:28 -0400

    finished writing the save function, need to write load function, and test both

--- TaskOrg/MainPage.xaml.cs
+++ TaskOrg/MainPage.xaml.cs
@@ -28,6 +28,19 @@ namespace TaskOrg
         static List<TaskList> listArray;
         int NewButtonID;
 
+
+        
+        StorageFolder localFolder = null;
+        int localCounter = 0;
+        StorageFolder localCacheFolder = null;
+        int localCacheCounter = 0;
+        StorageFolder roamingFolder = null;
+        int roamingCounter = 0;
+        StorageFolder temporaryFolder = null;
+        int temporaryCounter = 0;
+
+        const string filename = "data.txt";
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -36,12 +49,12 @@ namespace TaskOrg
 
             listArray = new List<TaskList>();
 
-            //for (int i = 0; i < 3; i++)
-            //{
 
-            //    addList();
+            localFolder = ApplicationData.Current.LocalFolder;
+            localCacheFolder = ApplicationData.Current.LocalCacheFolder;
+            roamingFolder = ApplicationData.Current.RoamingFolder;
+            temporaryFolder = ApplicationData.Current.TemporaryFolder;
 
-            //}
 
         }
         public void addList()
@@ -123,6 +136,142 @@ namespace TaskOrg
             deferral.Complete();
         }
 
+
+        async void Increment_Local_Click(Object sender, RoutedEventArgs e)
+        {
+            localCounter++;
+
+            StorageFile file = await localFolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, localCounter.ToString());
+
+
+
+            Read_Local_Counter();
+        }
+
+        async void Read_Local_Counter()
+        {
+            try
+            {
+                List<TaskList> temp = new List<TaskList>();
+                StorageFile file = await localFolder.GetFileAsync(filename);
+                IList<string> text = await FileIO.ReadLinesAsync(file);
+                for (int j = 0; j < text.Count; j++)
+                {
+                    string input = text[j];
+                    String[] tokens = input.Split('|');
+
+                    TaskList tempList = new TaskList();
+                    List<task> tasks = new List<task>();
+                    for (int i = 0; i < tokens.Count(); i++)
+                    {   
+                        if (i == 0)
+                        {
+                            tempList.Title = tokens[i];
+                        }
+                        else
+                        {
+                            String[] taskTokens = tokens[i].Split(',');
+                            task newTask = new task();
+                            newTask.Title = taskTokens[0];
+                            newTask.Description = taskTokens[1];
+                            tasks.Add(newTask);
+                        }
+                    }
+                    tempList.Tasks = tasks;
+                    temp.Add(tempList);
+                }
+                listArray = temp;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public static void loadOld()
+        {
+            StorageFolder local = ApplicationData.Current.LocalFolder;
+
+            string name = "data.txt";
+            StorageFile manifestFile = (StorageFile)(local.GetFileAsync(name));
+
+
+
+
+            // Specify the file path and options.
+            using (var isoFileStream =
+                    new System.IO.IsolatedStorage.IsolatedStorageFileStream
+                        ("DataFolder\\data.txt", System.IO.FileMode.Open, local))
+            {
+                // Read the data.
+                using (var isoFileReader = new System.IO.StreamReader(isoFileStream))
+                {
+                    while (!isoFileReader.EndOfStream)
+                    {
+                        String input = isoFileReader.ReadLine();
+                        String[] tokens = input.Split('|');
+                        for (int i = 0; i < tokens.Count(); i++)
+                        {
+                            TaskList tempList = new TaskList();
+                            List<task> tasks = new List<task>();
+                            if (i == 0)
+                            {
+                                tempList.Title = tokens[i];
+                            }
+                            else
+                            {
+                                String[] taskTokens = tokens[i].Split(',');
+                                task newTask = new task();
+                                newTask.Title = taskTokens[0];
+                                newTask.Description = taskTokens[1];
+                                tasks.Add(newTask);
+                            }
+                        }
+                    }
+                }
+            }
+
+            listArray = temp;
+        }
+        /// <summary>
+        /// Writes the content of the open flashcards to a file called name.txt
+        /// </summary>
+        /// <param name="name">The file name to use, will add a .txt to end of string</param>
+        public static void saveCurrent()
+        {
+            System.IO.IsolatedStorage.IsolatedStorageFile local =
+                System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication();
+
+            if (!local.DirectoryExists("DataFolder"))
+                local.CreateDirectory("DataFolder");
+
+            using (var isoFileStream =
+            new System.IO.IsolatedStorage.IsolatedStorageFileStream(
+                "DataFolder\\data.txt",
+                System.IO.FileMode.Create,
+                    local))
+            {
+                // Write the data from the textbox.
+                using (var isoFileWriter = new System.IO.StreamWriter(isoFileStream))
+                {
+                    int size = listArray.Count();
+                    for (int i = 0; i < size; i++)
+                    {
+                        TaskList currList = listArray[i];
+                        isoFileWriter.WriteLine(currList.Title);
+                        for (int k = 0; i < currList.Tasks.Count(); k++)
+                        {
+                            task currTask = currList.Tasks[k];
+                            isoFileWriter.Write("|" + currTask.Title + "," + currTask.Description);
+                        }
+                    }
+
+                }
+            }
+
+
+        }
 
     }
 }
